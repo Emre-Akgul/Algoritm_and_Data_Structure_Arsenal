3676c472ee7dc6ec489a1e466d65f8240868a518
Emre <Emreakgulcs@gmail.com>
2022-07-29 12:21:49 +0300

    Dijkstras algorithm
    I added Dijkstra's shortest path algorithm. Currently it is a relatively slow version of algorithm. I will add fast implementation and version that returns path.

---
+++ Algorithms/Dijkstra/Dijkstra.java
@@ -0,0 +1,132 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * Dijkstra
+ * 
+ * This algorithm takes graph as adjaceny matrix in the data structure int[][].
+ * Then calculates shortest path from first vertex to the each vertex in the graph.
+ * Algorithm works in both directed and undirected graphs. 
+ * Graph can include circles but cannot include negative edge length.
+ * 
+ * 
+ */
+public class Dijkstra {
+
+    static int INF = 1000000;
+    public static void main(String[] args) throws FileNotFoundException {
+        
+        int[][] graph = readIntegerFromFile("graph.txt");
+
+        int[] distances = new int[200];
+        boolean[] isFound = new boolean[200];
+
+        for(int i = 1; i < distances.length; i++){
+            distances[i] = INF;
+        }
+
+        isFound[0] = true;
+        distances[0] = 0;
+
+
+
+        for(int i = 0; i < graph.length - 1; i++){
+            int[] minArr = findMin(graph, distances, isFound);
+            isFound[minArr[0]] = true;
+            distances[minArr[0]] = minArr[1];
+            
+        }
+
+
+        printResults(distances);
+
+
+
+    
+    }
+
+    private static void printResults(int[] distances) {
+        for(int i = 0; i < distances.length; i++){
+            System.out.println("Distances from 0 to " + i + " is : " + distances[i]);
+        }
+    }
+
+    public static int[] findMin(int[][] graph, int[] distances, boolean[] isFound){
+
+        int[] res = {0,INF};
+        for(int i = 0; i < isFound.length; i++){
+            if(isFound[i]){
+                for(int j = 0; j < graph[i].length; j++){
+                    if(!isFound[j] && graph[i][j] < 1000000){
+                        int[] current = {j,distances[i] + graph[i][j]};
+                        if(current[1] < res[1]){
+                            res = current;
+                        }
+                    }
+                }
+            }            
+        }
+
+        return res;
+
+    }
+
+
+
+    public static void printGraph(int[][] arrs){
+        
+        for(int[] arr : arrs){
+            System.out.println(Arrays.toString(arr));
+        }
+
+    }
+    
+    
+    
+    public static int[][] readIntegerFromFile(String fileName){
+        int[][] result = new int[200][200];
+        for(int i = 0; i < result.length ; i++){
+            for(int j = 0; j < result[0].length ; j++){
+                result[i][j] = INF;
+            }
+        }
+
+
+        try {
+            File myObj = new File(fileName);
+            Scanner myReader = new Scanner(myObj);
+            int count = 0;
+            while (myReader.hasNext()) {
+                String str = myReader.next();
+                
+                if(isNumeric(str)){
+                    count = Integer.parseInt(str) - 1;
+                }
+                else{
+                    result[count][Integer.parseInt(str.substring(0,str.indexOf(",")))-1] = Integer.parseInt(str.substring(str.indexOf(",")+1));
+                }
+
+
+            }
+            myReader.close();
+        }catch (FileNotFoundException e) {
+            System.out.println("Aga dosya yok");
+        }
+        return result;
+
+    }
+
+    public static boolean isNumeric(String strNum) {
+        if (strNum == null) {
+            return false;
+        }
+        try {
+            double d = Double.parseDouble(strNum);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
+    }
+}
